c00485a4742e8f4269e72ecb32a87c8f417604a6
Hashan Lakruwan <81254452+Hashan2kk2@users.noreply.github.com>
2024-03-10 12:23:20 +0530

    complete code with comments

--- src/main/java/com/sky/learning/Main.java
+++ src/main/java/com/sky/learning/Main.java
@@ -6,17 +6,26 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Create a Scanner object for user input
         Scanner s = new Scanner(System.in);
+
+        // Prompt the user to enter the first number
         System.out.println("Please enter number 1");
         double num1 = s.nextDouble();
+
+        // Prompt the user to enter the second number
         System.out.println("Please enter number 2");
         double num2 = s.nextDouble();
 
+        // Prompt the user to enter an operator (+, -, *, /)
         System.out.println("Please enter the operator (+,-,*,/) :");
         char operator = s.next().charAt(0);
+
+        // Close the Scanner object to prevent resource leaks
         s.close();
 
         double output;
+        // Perform the calculation based on the operator entered
         switch (operator) {
             case '+':
                 output = num1 + num2;
@@ -34,7 +43,9 @@ public class Main {
                 System.out.println("Please Enter Valid Operator");
                 return;
         }
+
+        // Display the final result of the calculation
         System.out.println("=========== Final Result ===========");
-        System.out.println(num1 + " " + operator + " " + num2 + "=" + output);
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + output);
     }
 }
